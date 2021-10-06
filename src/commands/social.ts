83916e13bf7ff90e7cbf41dda9139391e3000462
Gerson Emanuel <67064502+Zepelino@users.noreply.github.com>
2021-10-06 15:20:51 -0300

    Adiciona a opção Discord no slash command /social (#3)
    * Adicionada a opção Discord
    
    Foi adicionada a opção Discord no slash command /social com um convite para o servidor
    
    * Update src/commands/social.ts
    
    Co-authored-by: Welton Felix <contas.weltonf@gmail.com>
    
    * Corrige erro de digitação
    
    Co-authored-by: Ryan Paiva <57689401+ryangpaiva@users.noreply.github.com>
    Co-authored-by: Welton Felix <contas.weltonf@gmail.com>

--- src/commands/social.ts
+++ src/commands/social.ts
@@ -17,6 +17,7 @@ export default {
           ['Instagram', 'instagram'],
           ['WhatsApp', 'whatsapp'],
           ['LinkedIn', 'linkedin'],
+          ['Discord', 'discord'],
         ])
         .setRequired(true),
     ),
@@ -48,6 +49,11 @@ export default {
       case 'linkedin':
         await interaction.reply(
           'LinkedIn do IFPE Open Source:\nhttps://www.linkedin.com/company/ifpe-open-source',
+        );
+        break;
+      case 'discord':
+        await interaction.reply(
+          'Servidor no Discord do IFPE Open Source: \nhttps://discord.gg/QjvN9HRA6C',
         );
         break;
       default:
